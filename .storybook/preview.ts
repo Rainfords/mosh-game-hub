f5d61de7e8d76b46eb32305d52076202a37a20e2
Ian Rainford <ian@rainfords.net>
2023-08-20 18:14:01 +1200

    Add Mock data

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,7 +1,16 @@
 import type { Preview } from "@storybook/react";
 import theme from "../src/theme";
+import { initialize, mswDecorator } from "msw-storybook-addon";
+
+/*
+ * Initializes MSW
+ * See https://github.com/mswjs/msw-storybook-addon#configuring-msw
+ * to learn how to customize it
+ */
+initialize();
 
 const preview: Preview = {
+  decorators: [mswDecorator],
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
